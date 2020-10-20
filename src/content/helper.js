cf95fa3c4cb52e5202ccbf3414e4717e47f1fc39
WebEXP0528 <app.dev0528@gmail.com>
2020-10-20 04:29:21 -0700

    Minor fixes

--- src/content/helper.js
+++ src/content/helper.js
@@ -1,48 +1,134 @@
-const wrapperSelector = `li>div.ember-view[class*="-result"]`;
+/**
+ * Get Search Results
+ *
+ * @param {HTMLElement} parent
+ */
+export const getSearchResultWrappers = (parent = document) => {
+    const wrapperSelector = `li>div.ember-view[class*="-result"]`;
+    return Array.from(parent.querySelectorAll(wrapperSelector));
+};
 
-const linkSelector = "a.ember-view";
-const imageSelector = "div.ivm-image-view-model.ember-view img";
+/**
+ * Check if people has shared connections
+ *
+ * @param {HTMLElement} parent
+ */
+export const isSharedConnection = (parent = document) => {
+    return parent.innerHTML.indexOf("shared connection") !== -1;
+};
 
-// const sharedConnSelector = "div.search-result__social-proof";
+/**
+ * Get Profile image
+ *
+ * @param {HTMLElement} parent
+ */
+export const getImage = (parent = document) => {
+    const imageSelector = "div.ivm-image-view-model.ember-view img";
+    return parent.querySelector(imageSelector);
+};
 
-const nextBtnSelector =
-    "button.artdeco-pagination__button.artdeco-pagination__button--next";
+/**
+ * Get Profile Link element
+ *
+ * @param {HTMLElement} parent
+ */
+export const getProfileLink = (parent = document) => {
+    const linkSelector = "a.ember-view";
+    return parent.querySelector(linkSelector);
+};
 
-const conBtnSelector = "button.pv-s-profile-actions--connect";
-const moreConBtnSelector =
-    "li>div>.artdeco-dropdown__item.pv-s-profile-actions--connect";
-const moreBtnSelector =
-    'button.pv-s-profile-actions__overflow-toggle.artdeco-button.artdeco-button--2[aria-expanded="false"]';
+export const delay = interval => {
+    return new Promise(resolve => setTimeout(resolve, interval));
+};
 
-const customMSGSelector = "textarea.send-invite__custom-message";
-const addNoteBtnSelector = 'button.artdeco-button[aria-label="Add a note"]';
-const inviteBtnSelector = 'button.artdeco-button[aria-label="Send now"]';
-const doneBtnSelector = 'button.artdeco-button[aria-label="Done"]';
-const sendInvitationBtnSelector =
-    'button.artdeco-button[aria-label="Send Invitation"]';
+/**
+ * Check if is invited already
+ *
+ * @param {HTMLElement} parent
+ */
+export const isInvited = (parent = document) => {
+    return (
+        parent.documentElement.innerHTML.indexOf(
+            "invitation has been sent to"
+        ) != -1 ||
+        parent.documentElement.innerHTML.indexOf("Remove Connection") != -1
+    );
+};
 
+/**
+ * Get ConnectButton
+ *
+ * @param {HTMLElement} parent
+ */
+export const getConnectBtn = (parent = document) => {
+    const conBtnSelector = "button.pv-s-profile-actions--connect";
+    const moreConBtnSelector =
+        "li>div>.artdeco-dropdown__item.pv-s-profile-actions--connect";
+    return (
+        parent.querySelector(conBtnSelector) ||
+        parent.querySelector(moreConBtnSelector)
+    );
+};
+
+/**
+ * Get Custom Message View Element
+ *
+ * @param {HTMLElement} parent
+ */
+export const getCustomMessageView = (parent = document) => {
+    const customMSGSelector = "textarea.send-invite__custom-message";
+    return parent.querySelector(customMSGSelector);
+};
+
+/**
+ * Get Add Note Button Element
+ *
+ * @param {HTMLElement} parent
+ */
+export const getAddNoteBtn = (parent = document) => {
+    const addNoteBtnSelector = 'button.artdeco-button[aria-label="Add a note"]';
+    return parent.querySelector(addNoteBtnSelector);
+};
+
+/**
+ * Get Invite Button Element
+ *
+ * @param {HTMLElement} parent
+ */
+export const getInviteBtn = (parent = document) => {
+    const inviteBtnSelector = 'button.artdeco-button[aria-label="Send now"]';
+    const inviteBtnSelector1 = 'button.artdeco-button[aria-label="Done"]';
+    const inviteBtnSelector2 =
+        'button.artdeco-button[aria-label="Send Invitation"]';
+    return (
+        parent.querySelector(inviteBtnSelector) ||
+        parent.querySelector(inviteBtnSelector1) ||
+        parent.querySelector(inviteBtnSelector2)
+    );
+};
+
+/**
+ * Get peoples from search page
+ *
+ * @param {{includeMutual:boolean, includePhoto:boolean}} config
+ */
 export const getPeopleFromSearchPage = config => {
-    var wrappers = Array.from(document.querySelectorAll(wrapperSelector));
+    var wrappers = getSearchResultWrappers();
     const filtered = wrappers
         .filter(wrapper => {
             const shardConn = config.includeMutual
                 ? true
-                : wrapper.innerHTML.indexOf("shared connection") == -1;
-            const image = config.includePhoto
-                ? true
-                : wrapper.querySelector(imageSelector);
-            // console.log(wrapper.querySelector(imageSelector));
+                : !isSharedConnection(wrapper);
+            const image = config.includePhoto ? true : getImage(wrapper);
             return (
                 image &&
                 shardConn &&
-                !wrapper
-                    .querySelector(linkSelector)
-                    .classList.contains("disabled")
+                !getProfileLink(wrapper).classList.contains("disabled")
             );
         })
         .map(item => {
-            const link = item.querySelector(linkSelector);
-            const image = item.querySelector(imageSelector);
+            const link = getProfileLink(item);
+            const image = getImage(item);
             return {
                 url: link.getAttribute("href"),
                 name: "",
@@ -51,74 +137,66 @@ export const getPeopleFromSearchPage = config => {
         });
 
     console.log("~~~~~~~~~~~~ filtered peoples", filtered);
-
     return filtered;
 };
 
-export const delay = interval => {
-    return new Promise(resolve => setTimeout(resolve, interval));
-};
-
+/**
+ * Invite People
+ *
+ * @param {string} msg Invitation Message
+ */
 export const invitePeople = msg => {
     return new Promise(async (resolve, reject) => {
         try {
-            await delay(10 * 1000);
+            await delay(10 * 1000); // Delay for 10 seconds
 
             // if invitation is pending
-            if (
-                document.documentElement.innerHTML.indexOf(
-                    "invitation has been sent to"
-                ) != -1 ||
-                document.documentElement.innerHTML.indexOf(
-                    "Remove Connection"
-                ) != -1
-            ) {
+            if (isInvited()) {
                 console.log("~~~~~ Already invited! ~~~~~");
                 return resolve(false);
             }
 
             // connect button
-            let connectButton = querySelector(conBtnSelector);
+            let connectButton = getConnectBtn();
             if (!connectButton) {
-                //more connect button
-                connectButton = querySelector(moreConBtnSelector);
-                if (!connectButton) {
-                    console.log("~~~~~ Could not found connect button ~~~~~");
-                    return resolve(false);
-                }
+                console.log("~~~~~ Could not found connect button ~~~~~");
+                return resolve(false);
             }
-            connectButton.click();
-            await delay(1 * 1000);
-            if (!msg || msg == "" || msg == " ") {
-                querySelector(inviteBtnSelector).click();
-            } else {
+            connectButton.click(); // Click connect button
+
+            await delay(1 * 1000); // Delay for 1 second
+
+            if (msg) {
                 // custom message
-                let customMSGEle = querySelector(customMSGSelector);
+                let customMSGEle = getCustomMessageView();
                 if (!customMSGEle) {
-                    // addnote button
-                    const addNoteButton = querySelector(addNoteBtnSelector);
+                    // Click Add Note button if customMessageView non exist
+                    const addNoteButton = getAddNoteBtn();
                     if (!addNoteButton) {
-                        console.log(
-                            "~~~~~ Could not found Add note button ~~~~~"
-                        );
                         return resolve(false);
                     }
                     addNoteButton.click();
                     await delay(1 * 1000);
-                    customMSGEle = querySelector(customMSGSelector);
+                    customMSGEle = getCustomMessageView();
                 }
+
+                //Insert message in customMessageView
                 customMSGEle.value = msg;
-                var evt = document.createEvent("Events"); //Add change event in textarea
+                var evt = document.createEvent("Events"); // Add change event in textarea
                 evt.initEvent("change", true, true);
                 customMSGEle.dispatchEvent(evt);
-                await delay(1 * 1000);
-                // invite button
-                if (querySelector(doneBtnSelector)) {
-                    querySelector(doneBtnSelector).click();
-                } else if (querySelector(sendInvitationBtnSelector)) {
-                    querySelector(sendInvitationBtnSelector).click();
-                }
+
+                await delay(1 * 1000); // Delay for 1 seconds
             }
+            // Click Invite Button
+            const inviteBtn = getInviteBtn();
+            console.log(`~~~~ inviteBtn`, inviteBtn);
+            if (!inviteBtn) {
+                return resolve(false);
+            }
+            inviteBtn.click();
+
+            await delay(1 * 1000); // Delay for 1 seconds
 
             return resolve(true);
         } catch (err) {
@@ -127,14 +205,30 @@ export const invitePeople = msg => {
     });
 };
 
-export const querySelector = selector => {
-    return document.querySelector(selector);
+/**
+ * Get Element from parent
+ *
+ * @param {string} selector
+ * @param {HTMLElement} parent
+ */
+export const querySelector = (selector, parent = document) => {
+    return parent.querySelector(selector);
 };
 
+/**
+ * Click Next page button
+ */
 export const nextSearchPage = () => {
+    const nextBtnSelector =
+        "button.artdeco-pagination__button.artdeco-pagination__button--next";
     querySelector(nextBtnSelector).click();
 };
 
+/**
+ * Create LocalStorage Instance
+ *
+ * @param {*} storageProp
+ */
 export const createLocalStorageAccess = storageProp => {
     const propId = `linkedinAutomation___${storageProp}`;
     return {
